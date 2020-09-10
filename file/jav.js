93fd399b30e150e5850cc912fc02af10643ea74f
Y0rFa1se <Y0rFa1se@users.noreply.github.com>
2020-09-10 16:13:12 +0900

    ddd

--- file/jav.js
+++ file/jav.js
@@ -2,7 +2,7 @@ function pwcheck(){
   var inputVal = document.getElementById("pw").value;
 
   if (inputVal == "dldmswnstkfkdgo") {
-    window.location = 'https://drive.google.com/file/d/11A5kUBsUZr4pNGU-Mdg4ZKiZHpGUKOp9/view';
+    window.location = 'https://drive.google.com/file/d/1ejpIr-12q701R1XJtOmh-VLIXJvnk1aW/view?usp=sharing';
   } else {
     alert("어림도없지");
     window.history.back();
